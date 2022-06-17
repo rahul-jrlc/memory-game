8fc92356b2c9abf65c4e5109ed70d36d3d969c9f
Rahul <rahul.pandita41299@gmail.com>
2022-06-18 00:36:38 +0530

    Create card array

--- js/script.js
+++ js/script.js
@@ -3,3 +3,52 @@
 document.addEventListener("DOMContentLoaded", function(){  
     // CARD OPTIONS
 })
+
+
+
+// ARRAY FOR THE CARDS DISPLAYED ON GRID
+const cardArray = [
+    {
+        name: "fries",
+        img: "../img/fries.png" 
+    },
+    {
+        name: "cheeseburger",
+        img: "../img/cheeseburger.png" 
+    },
+    {
+        name: "hotdog",
+        img: "../img/hotdog.png" 
+    },
+    {
+        name: "ice-cream",
+        img: "../img/ice-cream.png" 
+    },
+    {
+        name: "pizza",
+        img: "../img/pizza.png" 
+    },
+    {
+        name: "milkshake",
+        img: "../img/milkshake.png" 
+    },
+    {
+        name: "white",
+        img: "../img/white.png" 
+    },
+    {
+        name: "blank",
+        img: "../img/blank.png" 
+    }
+
+]  
+
+// const gameboard = document.querySelector("#grid") // GAMEBOARD GRID
+
+// // CREATE GAMEBOARD
+// function gameBoard() {
+//     for (let i = 0; i < cardArray.length; i++) {
+//         let card = document.createElement("img") // CREATE IMAGE ELEMENT FOR EACH CARD
+//         card.setAttribute("src")
+//     }
+// }
